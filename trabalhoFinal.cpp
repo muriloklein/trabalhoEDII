6a0e4b8cd6d6be69f4aa18714757cc2a1149d405
Murilo Klein <murilo.kkleinn@gmail.com>
2024-05-20 20:04:52 -0300

    Adicionado estrutura de nós da árvore de Huffman e função de comparação (teste)

--- trabalhoFinal.cpp
+++ trabalhoFinal.cpp
@@ -15,6 +15,20 @@ map<wchar_t, int> analyzeFile(const wstring &filename) {
     return frequencies;
 }
 
+struct HuffmanNode {
+    wchar_t symbol;
+    int frequency;
+    HuffmanNode *left;
+    HuffmanNode *right;
+    HuffmanNode(wchar_t s, int f) : symbol(s), frequency(f), left(nullptr), right(nullptr) {}
+};
+
+struct compare {
+    bool operator()(HuffmanNode* l, HuffmanNode* r) {
+        return (l->frequency > r->frequency);
+    }
+};
+
 int main() {
     wstring filename = "input.txt";
     map<wchar_t, int> frequencies = analyzeFile(filename);
